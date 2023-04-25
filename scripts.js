2abf196130a94a993c8b772f2a2233db08801c1d
Subba Codes <subba.codes@gmail.com>
2023-04-25 18:14:14 +0900

    added javascript content

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
 
+const generateTarget = () => {
+  let secretTargetNumber = Math.floor(Math.random() * 10);
+  return secretTargetNumber;
+};
+
+// console.log(generateTarget());
+
+const compareGuesses = (humanGuess, computerGuess, secretNum) => {
+  if (humanGuess === computerGuess) return true;
+  else if (humanGuess - secretNum < computerGuess - secretNum) return true;
+  else if (computerGuess === secretNum) return false;
+  // else return false
+};
+
+const updateScore = winner => {
+  if (winner === "human") return (humanScore += 1);
+  else if (winner === "computer") return (computerScore += 1);
+};
+
+const advanceRound = () => {
+  return (currentRoundNumber += 1);
+};
